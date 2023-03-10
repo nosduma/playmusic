8aca04290176bdb6f59c5077c28b3914f245caec
Noscelo <nosduma021@student.wethinkcode.co.za>
2023-03-10 19:02:52 +0200

    complete

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
 
 // Song titles
-const song = ['Carry On' , 'Your Song', 'Desoleil', 'Sete']
+const song = ['fun' , 'yoursong', 'desoleil', 'sete']
 
 // tracking the songs
 let songIndex = 1
@@ -24,7 +24,97 @@ function loadSong(song){
     cover.src = 'images/${song}.jpg'
 }
 
-playBtn.addEventListener('click', ())
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+
+
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+
+
+}
+
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = song.length - 1
+    }
+
+    loadSong(song[songIndex])
+
+    playSong()
+
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > song.length- 1) {
+        songIndex = 0
+    }
+
+    loadSong(song[songIndex])
+
+    playSong()
+
+
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime/duration)*100
+    progress.style.width = '${progressPercent}%'
+
+
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    // console.log(width);
+    const clickX = e.offsetX
+    // 
+    const duration = audio.duration
+
+    audio.currentTime = (clickX/width)*duration
+
+
+}
+
+// Event listeners
+playBtn.addEventListener('click', () =>{
+    const isPlaying =musicContainer.classList.contains('play')
+
+    if (isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+}) 
+
+// Change song events
+prevBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate',updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
+
+
+
+
 
 
 
